4bed75a750cea0bce95f35c9f7a2b8529d0de8ea
nn-fiha <jucse28.361@gmail.com>
2023-01-16 21:59:27 +0600

    add alert

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 let count = 0;
 
-function setCount() {
-  document.getElementById("output").innerText = count;
+function setCount(){
+    document.getElementById("output").innerText = count;
 }
 
-function setInitialCount() {
-  setCount();
+function setInitialCount(){
+    
+    setCount();
 }
 
-function increment() {
-  count++;
-  setCount();
-}
+function increment(){
+ count++;
+ setCount();
+ if(count > 10) {
+    alert("stop!!!");
+ }
 
+ 
+
+}
 setInitialCount();
