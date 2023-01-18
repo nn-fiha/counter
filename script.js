815dbccca5971d2e974e3921147123aa1462bebe
kmtusher97 <kmtusher97@gmail.com>
2023-01-18 21:54:49 +0600

    init build list app

--- script.js
+++ script.js
@@ -1,38 +1,16 @@
-function setInitialValue() {
-  document.getElementById("marks-inp").value = 0;
+function getInput() {
+  return document.getElementById("input-box").value;
 }
 
-function showGrade(grade) {
-  document.getElementById("grade").innerText = grade;
+function createELement(text) {
+  const item = document.createElement("h2");
+  item.innerText = text;
+  return item;
 }
 
-function isValidateIuput(input) {
-  return (input >= 0 && input <= 100);
+function addItem() {
+  const inputText = getInput();
+  const item = createELement(inputText);
+  const targetElement = document.getElementById("list");
+  targetElement.appendChild(item);
 }
-
-function calculateGrade() {
-  const marks = parseInt(document.getElementById("marks-inp").value);
-
-  if (!isValidateIuput(marks)) {
-    alert("Invalid input");
-    return;
-  } 
-
-  if (marks >= 80 && marks <= 100) {
-    showGrade("A+");
-  } else if (marks >= 70 && marks < 80) {
-    showGrade("A");
-  } else if (marks >= 60 && marks < 70) {
-    showGrade("A-");
-  } else if (marks >= 50 && marks < 60) {
-    showGrade("B");
-  } else if (marks >= 40 && marks < 50) {
-    showGrade("C");
-  } else if (marks >= 33 && marks < 40) {
-    showGrade("D");
-  } else if (marks >= 0 && marks < 33) {
-    showGrade("F");
-  }
-}
-
-setInitialValue();
