c0f5eac2cc432c5cb0b95dd1a396f3dc974d6a90
kmtusher97 <kmtusher97@gmail.com>
2023-01-18 21:09:57 +0600

    format code and add functions

--- script.js
+++ script.js
@@ -2,32 +2,36 @@ function setInitialValue() {
   document.getElementById("marks-inp").value = 0;
 }
 
+function showGrade(grade) {
+  document.getElementById("grade").innerText = grade;
+}
+
+function isValidateIuput(input) {
+  return (input >= 0 && input <= 100);
+}
+
 function calculateGrade() {
   const marks = parseInt(document.getElementById("marks-inp").value);
-  
+
+  if (!isValidateIuput(marks)) {
+    alert("Invalid input");
+    return;
+  } 
+
   if (marks >= 80 && marks <= 100) {
-    document.getElementById("grade").innerText = "A+";
-  }
-  else if (marks >= 70 && marks <80) {
-    document.getElementById("grade").innerText = "A";
-  }
-  else if (marks >= 60 && marks <70) {
-    document.getElementById("grade").innerText = "A-";
-  }
-  else if (marks >= 50 && marks <60) {
-    document.getElementById("grade").innerText = "B";
-  }
-  else if (marks >= 40 && marks <50) {
-    document.getElementById("grade").innerText = "C";
-  }
-  else if (marks >= 33 && marks <40) {
-    document.getElementById("grade").innerText = "D";
-  }
-  else if (marks >= 0 && marks <33) {
-    document.getElementById("grade").innerText = "F";
-  }
-  else if (marks <0 || marks >100) {
-    alert("Please check the mark again!!!")
+    showGrade("A+");
+  } else if (marks >= 70 && marks < 80) {
+    showGrade("A");
+  } else if (marks >= 60 && marks < 70) {
+    showGrade("A-");
+  } else if (marks >= 50 && marks < 60) {
+    showGrade("B");
+  } else if (marks >= 40 && marks < 50) {
+    showGrade("C");
+  } else if (marks >= 33 && marks < 40) {
+    showGrade("D");
+  } else if (marks >= 0 && marks < 33) {
+    showGrade("F");
   }
 }
 
